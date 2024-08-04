53354e43d1e11c568ffb5067883d807b799e09e2
Ba_Nguyen <vanbavlkt.56@gmail.com>
2024-08-04 22:29:10 +0700

    update thread

--- app/src/main/java/com/example/appdemo/ui/MainViewModel.kt
+++ app/src/main/java/com/example/appdemo/ui/MainViewModel.kt
@@ -1,16 +1,19 @@
 package com.example.appdemo.ui
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.appdemo.data.model.Stocks
 import com.example.appdemo.data.repository.IMainRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,7 +22,7 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val mainRepo: IMainRepo) : ViewModel() {
 
     private val data: StateFlow<List<Stocks>> =
-        mainRepo.getData().stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+        mainRepo.getData().flowOn(Dispatchers.IO).stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
     private val _searchQuery = MutableStateFlow("")
     private val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
     private val _isRetry = mutableStateOf(false)
@@ -31,7 +34,7 @@ class MainViewModel @Inject constructor(private val mainRepo: IMainRepo) : ViewM
         } else {
             data.filter { it.name.contains(query, ignoreCase = true) }
         }
-    }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+    }.flowOn(Dispatchers.IO).stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
 
     fun updateSearchQuery(query: String) {
         _searchQuery.value = query
